8b53d34582585ff62e8270ada4b65dd3f4f9e812
Amadeo Morán <amadeo.moran@gmail.com>
2023-11-11 12:44:57 +0100

    ni

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,62 +16,57 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const EDGEDB_AUTH_BASE_URL = "http://localhost:10702/db/edgedb/ext/auth/"
+export const EDGEDB_AUTH_BASE_URL =
+  "http://localhost:10723/db/edgedb/ext/auth/ui";
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   const session = await getSession(request.headers.get("Cookie"));
   console.log("session.get(challenge) -> ", session.has("challenge"));
-  
   const challenge = initiatePKCE(session)
 
-  const redirectUrl = new URL(
-    "signin",
-    EDGEDB_AUTH_BASE_URL
-  );
 
-  const email = 'test@test.es'
-  const password = 'testtest'
-  const provider = 'Email + Password'
+  const redirectUrl = new URL("signin", EDGEDB_AUTH_BASE_URL);
 
-  console.log(redirectUrl.href)
-  const response = await fetch(
-    redirectUrl.href,
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email,
-        password,
-        verify_url: '/signup',
-        provider,
-        challenge,
-      }),
-    }
-  );
+  const email = "test@test.es";
+  const password = "testtest";
+  const provider = "Email + Password";
 
-  console.log(response)
-  console.log(await response.json())
+  console.log(redirectUrl.href);
+  const response = await fetch(redirectUrl.href, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email,
+      password,
+      verify_url: "/signup",
+      provider,
+      challenge,
+    }),
+  });
+
+  console.log(response);
+  console.log(await response.json());
   if (!response.ok) {
     throw new Error("Oh no!");
   }
 
   const { code } = await response.json();
 
-  console.log(code)
+  console.log("CODE -> ", code);
 
   // redirectUrl.searchParams.set("challenge", challenge);
 
   // return redirect(redirectUrl.href);
   return json({
-    code
-  })
+    code,
+  });
 };
 
 export async function action({ request }: ActionFunctionArgs) {
   const session = await getSession(request.headers.get("Cookie"));
 
   session.set("challenge", "klsjadfasdlfjl");
-  console.log(JSON.stringify(session.get('challenge')));
+  console.log(JSON.stringify(session.get("challenge")));
 
   // Login succeeded, send them to the home page.
   return redirect("/signing", {
@@ -82,12 +77,12 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function Index() {
-  const { code } = useLoaderData<typeof loader>()
+  const { code } = useLoaderData<typeof loader>();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <h2>{code ?? 'no hay code'}</h2>
+      <h2>{code ?? "no hay code"}</h2>
       <form method="POST" action="/?index">
         <button type="submit">AQui</button>
       </form>
@@ -96,14 +91,14 @@ export default function Index() {
 }
 
 function initiatePKCE(session: Session) {
-  const verifier = crypto.randomBytes(32).toString("base64url")
+  const verifier = crypto.randomBytes(32).toString("base64url");
 
-  session.set("edgedb-pkce-verifier", verifier)
+  session.set("edgedb-pkce-verifier", verifier);
 
   const challenge = crypto
     .createHash("sha256")
     .update(verifier)
-    .digest("base64url")
+    .digest("base64url");
 
   return challenge
 }
